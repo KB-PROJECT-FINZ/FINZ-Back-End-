859f0d8c46e8d00e49188245f8ced60f8c924bf9
YeonWoo choi <rryunn@naver.com>
2025-07-24 22:21:42 +0900

    feat: cors 설정 추가

---
+++ src/main/java/org/scoula/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package org.scoula.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로
+                .allowedOrigins("http://localhost:5173") // 프론트엔드 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
