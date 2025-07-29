ae60ed47b38fea0d50266302434da8da50e3dd1a
CHOI-074 <tuffhrgha704@naver.com>
2025-07-29 15:32:07 +0900

    fix: 이메일인증 및 회원가입폼 오류 수정

---
+++ src/main/java/org/scoula/config/MailConfig.java
@@ -0,0 +1,29 @@
+package org.scoula.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender mailSender() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost("smtp.gmail.com");
+        sender.setPort(587);
+        sender.setUsername("your_email@gmail.com");
+        sender.setPassword("your_app_password");
+
+        Properties props = sender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return sender;
+    }
+}
