a42e4fae0544fe4cf9bb66c74d5dca6c306538f3
Hwang-Dawon <daaaawon97@gmail.com>
2025-08-12 11:46:38 +0900

    fix:전반적 오류 수정

--- src/main/java/org/scoula/domain/ranking/MyRankingDto.java
+++ src/main/java/org/scoula/domain/ranking/MyRankingDto.java
@@ -11,6 +11,7 @@ public class MyRankingDto {
     private Integer ranking;      // 전체 순위
     private Double topPercent;    // 상위 퍼센트 (%)
     private String riskType;     //성향
+    private String originalTrait;
     private String baseDate;   // 주간 랭킹 기준 날짜 추가
 
 
